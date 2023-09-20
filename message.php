9a18633cdd545f50d42b5601921fdd502aa053c4
Umeid Ijate <umeidijate2@gmail.com>
2023-09-20 13:49:32 +0200

    arquivos

---
+++ message.php
@@ -0,0 +1,26 @@
+//Contact Form in PHP
+<?php
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $phone = htmlspecialchars($_POST['phone']);
+  $website = htmlspecialchars($_POST['website']);
+  $message = htmlspecialchars($_POST['message']);
+
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "umeid.ijate@gmail.com"; //enter that email address where you want to receive all messages
+      $subject = "From: $name <$email>";//subject of the email address, subject looks like from: UmeidiIjate  <abc@gmail.com>
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $sender = "From: $email";//sender email
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Sua mensagem foi enviada";
+      }else{
+         echo "Desculpe, não foi possível enviar sua mensagem!";
+      }
+    }else{
+      echo "Digite um endereço de e-mail válido!";
+    }
+  }else{
+    echo "O campo e-mail e mensagem é obrigatório!";
+  }
+?>
---
+++ scripts.js
@@ -0,0 +1,31 @@
+//Contact Form in PHP
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=>{
+  e.preventDefault();//preventing form submitting
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Enviando sua mensagem...";
+  //form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest(); //creating new xml object
+  xhr.open("POST", "message.php", true);//sending post request to message.php file
+  xhr.onload = ()=>{//once ajax loaded
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("Campos email e Senha são obrigatórios!") != -1 || response.indexOf("Insira um endereço de e-mail válido!") || response.indexOf("O campo e-mail e mensagem é obrigatório!")){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      //form.classList.remove("disabled");
+    }
+  }
+  let formData = new FormData(form);//creating new formData obj. This obj is used to send form data
+  xhr.send(formData);//sending form data
+}
